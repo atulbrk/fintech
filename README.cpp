24cbed5143cd93403761aa170d7c4ca2c77b2a9a
atulbrk <43037298+atulbrk@users.noreply.github.com>
2021-09-04 09:56:35 +0530

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,1 +1,18 @@
-# fintech
+#include<iostream>
+using namespace std;
+class test()
+{
+  void test(float s,float t)
+{
+    cout << "Function with float called ";
+}
+void test(int s, int t)
+{
+    cout << "Function with int called ";
+}	
+};
+int main()
+{ test obj;
+    obj.test(3.5, 5.6);
+    return 0;
+}
