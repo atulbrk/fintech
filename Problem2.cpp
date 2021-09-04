c98ee69b39653f86e7098767cf2fb83001f39b0e
atulbrk <43037298+atulbrk@users.noreply.github.com>
2021-09-04 11:08:34 +0530

    Update Problem2.cpp

--- Problem2.cpp
+++ Problem2.cpp
@@ -1,1 +1,29 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	int s[n];
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>s[i];
+	}
+	sort(s,s+n);
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			for(int k=j+1;k<n;k++)
+			{
+				if(s[i]+s[j]+s[k]==0)
+				{
+					cout<<s[i]<<","<<s[j]<<","<<s[k]<<endl;
+				}
+			}
+		}
+	}
+	
+	
+	return 0;
+}
