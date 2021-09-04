1689cfdd1ef237bbbb263022d24b894c9825d979
atulbrk <43037298+atulbrk@users.noreply.github.com>
2021-09-04 10:51:05 +0530

    Update Problem3.cpp

--- Problem3.cpp
+++ Problem3.cpp
@@ -1,1 +1,27 @@
-
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+int main()
+{char str[20];
+  int c=0;  
+       cin>>str;
+       int n=strlen(str);
+    for(int i = 0; i < n/2; i++){  
+    
+        if(str[i] != str[n-i-1]){  
+            c = 1;  
+            break;  
+        }  
+    }  
+      
+    if(c==0)  
+       {
+       	 cout<<"True";
+	   }
+    else  
+       {
+	   cout<<"False";  
+        
+		 }  
+    return 0;  
+}  
